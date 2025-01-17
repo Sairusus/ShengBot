165f804d7dea6f0711cd6a43459b211207bf5159
Sairus Seng <sus7211@rit.edu>
2025-01-17 09:30:34 -0500

    added subcommands to starboard and deployed them

--- src/commands/starboard.js
+++ src/commands/starboard.js
@@ -1,10 +1,34 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, MessageFlags } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
     .setName("starboard")
-    .setDescription("Setup the starboard."),
+    .setDescription("Setup the starboard.")
+    .addSubcommand(subCommand => 
+        subCommand 
+            .setName("enable")
+            .setDescription("Enables the starboard output to a channel.")
+            .addStringOption(option => option.setName("channel-id").setDescription("Channel ID for starboard output.").setRequired(true))
+    )
+    .addSubcommand(subCommand => 
+        subCommand 
+            .setName("disable")
+            .setDescription("Disables the starboard.")
+    )
+    .addSubcommand(subCommand => 
+        subCommand 
+            .setName("set")
+            .setDescription("Sets a new channel for starboard output.")
+            .addStringOption(option => option.setName("channel-id").setDescription("Channel ID for starboard output.").setRequired(true))
+    )
+    .addSubcommand(subCommand => 
+        subCommand 
+            .setName("min")
+            .setDescription("Sets the minimum stars required to post in starboard.")
+            .addIntegerOption(option => option.setName("min-stars").setDescription("Minimum stars required for starboard.").setRequired(true))
+    )
+    ,
     async execute(interaction){
-        await interaction.reply("WIP!");
+        await interaction.reply({ content: "WIP!", flags: MessageFlags.Ephemeral });
     },
 }
