21cca562c653673648ef69d9ceccde6893ba0c0b
Sairus Seng <sus7211@rit.edu>
2025-01-17 16:13:21 -0500

    all starboard commands respond to the user

--- src/commands/starboard.js
+++ src/commands/starboard.js
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder, MessageFlags, Collection, ChannelType } from "discord.js";
 
 const serverStarboards = new Collection();
+const serverMinStars = new Collection();
 
 const enable = async (interaction) => {
     const channel = interaction.options.getChannel("channel")
@@ -10,29 +11,48 @@ const enable = async (interaction) => {
         await interaction.reply({ content: `${guild.name} already has a starboard. Use 'starboard set' to change the output channel.`, flags: MessageFlags.Ephemeral });
         return;
     }
-    
+
     serverStarboards.set(guild.id, channel.id);
 
     try {
         await interaction.reply({ content: `${channel.name} will be the starboard for ${guild.name}`, flags: MessageFlags.Ephemeral });
     }
     catch (error){
-        console.log(channel);
         console.log(error);
         await interaction.reply({ content: `An internal error has occured.`, flags: MessageFlags.Ephemeral });
     }
 }
 
 const disable = async (interaction) => {
-    
+    const guild = interaction.guild;
+    serverStarboards.delete(guild.id);
+    await interaction.reply({ content: `Starboard has been disabled.`, flags: MessageFlags.Ephemeral });
 }
 
 const set = async (interaction) => {
-    
+    const guild = interaction.guild;
+    const channel = interaction.options.getChannel("channel");
+    serverStarboards.set((guild.id, channel.id));
+    try {
+        await interaction.reply({ content: `${channel.name} will be the starboard for ${guild.name}`, flags: MessageFlags.Ephemeral });
+    }
+    catch (error){
+        console.log(error);
+        await interaction.reply({ content: `An internal error has occured.`, flags: MessageFlags.Ephemeral });
+    }
 }
 
 const min = async (interaction) => {
-    
+    const guild = interaction.guild;
+    const starCount = interaction.options.getInteger("min-stars");
+
+    if(!serverStarboards.get(guild.id)){
+        await interaction.reply({ content: `Starboard is not enabled. Use 'starboard enable'.`, flags: MessageFlags.Ephemeral });
+        return;
+    }
+
+    serverMinStars.set((guild.id, starCount));
+    await interaction.reply({ content: `${starCount} stars are now required to post in starboard.`, flags: MessageFlags.Ephemeral });
 }
 
 const subCommands = new Collection([
@@ -66,13 +86,22 @@ export default {
         subCommand 
             .setName("set")
             .setDescription("Sets a new channel for starboard output.")
-            .addStringOption(option => option.setName("channel-id").setDescription("Channel ID for starboard output.").setRequired(true))
+            .addChannelOption(option => 
+                option
+                .setName("channel")
+                .setDescription("Channel for starboard output.")
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText))
     )
     .addSubcommand(subCommand => 
         subCommand 
             .setName("min")
             .setDescription("Sets the minimum stars required to post in starboard.")
-            .addIntegerOption(option => option.setName("min-stars").setDescription("Minimum stars required for starboard.").setRequired(true))
+            .addIntegerOption(option => 
+                option
+                .setName("min-stars")
+                .setDescription("Minimum stars required for starboard.")
+                .setRequired(true))
     )
     ,
     async execute(interaction){
